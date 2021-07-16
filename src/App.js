ff9ef8aa887c689d54f625ea90eb8124cedeec59
user <user@iMac-user.local>
2021-07-16 17:16:25 +0300

    added usrReduser, userAction to header

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import "./App.css";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/heder/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import React from "react";
+
+import { setCurrentUser } from "./redux/user/user.actions";
+
+import "./App.css";
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((onSnapshot) => {
-          this.setState({
-            currentUser: {
-              id: onSnapshot.id,
-              ...onSnapshot.data(),
-            },
+          setCurrentUser({
+            id: onSnapshot.id,
+            ...onSnapshot.data(),
           });
         });
       }
+      setCurrentUser(userAuth);
     });
   }
 
@@ -52,4 +49,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
